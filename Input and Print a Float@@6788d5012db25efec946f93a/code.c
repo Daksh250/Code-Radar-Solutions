eb8037f061ef843966b4b0b7bcf3a123e7b4b8eb
Daksh250 <daksh9f11@gmail.com>
2025-01-27 23:15:34 +0530

    Solution to Input and Print a Float

---
+++ Input and Print a Float@@6788d5012db25efec946f93a/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    float n;
+    printf("");
+    scanf("%f,&n")
+    return 0;
+
+}
