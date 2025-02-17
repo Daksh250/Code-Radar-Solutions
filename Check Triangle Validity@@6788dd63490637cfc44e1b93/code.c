c1a2f9352283f0c85fdd2fa3c131665569030df4
Daksh <daksh9f11@gmail.com>
2025-02-18 01:47:41 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -1,22 +1,34 @@
+// #include<stdio.h>
+// int main(){
+//     int a,b,c;
+//     scanf("%d %d %d",&a,&b,&c);
+//     int d=(a*a)+(b*b);
+//     int e=(c*c);
+//     int f=(a*a)+(c*c);
+//     int g=(b*b);
+//     int h=(b*b)+(c*c);
+//     int i=(a*a);
+//     if(d==e){
+//         printf("Valid");
+
+//     }
+//     else if(f==g){
+//         printf("Valid");
+
+//     }
+//     else if(h==i){
+//         printf("Valid");
+//     }
+//     else{
+//         printf("Invalid");
+//     }
+// }
 #include<stdio.h>
+
 int main(){
     int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    int d=(a*a)+(b*b);
-    int e=(c*c);
-    int f=(a*a)+(c*c);
-    int g=(b*b);
-    int h=(b*b)+(c*c);
-    int i=(a*a);
-    if(d==e){
-        printf("Valid");
-
-    }
-    else if(f==g){
-        printf("Valid");
-
-    }
-    else if(h==i){
+    scanf("%d %d %d ",&a,&b,&c);
+    if(a+b>c && a+c>b && b+a>>c){
         printf("Valid");
     }
     else{
